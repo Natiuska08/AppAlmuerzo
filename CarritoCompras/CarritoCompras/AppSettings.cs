edaaf1ff7acdaa9b3ec551ce986a4b33bc58af12
natiu <natiu@LAPTOP-5SK8PJFN>
2022-06-05 10:08:13 -0500

    Agregar archivos de proyecto.

---
+++ CarritoCompras/CarritoCompras/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using AppNotas.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarritoCompras
+{
+    public class AppSettings
+    {
+        public static string ApiFirebase = "https://ventastest1-eb4f7-default-rtdb.firebaseio.com/";
+        private static string KeyAplication = "AIzaSyC6RYnv5iusMorIuMYvCwWD_WIHrDHiwtE";
+
+
+        public static ResponseAuthentication oAuthentication { get; set; }
+        private static string ApiUrlGoogleApis = "https://identitytoolkit.googleapis.com/v1/";
+
+        public static string ApiAuthentication(string tipo)
+        {
+            if (tipo == "LOGIN")
+                return ApiUrlGoogleApis + "accounts:signInWithPassword?key=" + KeyAplication;
+            else if (tipo == "SIGNIN")
+                return ApiUrlGoogleApis + "accounts:signUp?key=" + KeyAplication;
+            else
+                return "";
+        }
+
+    }
+}
